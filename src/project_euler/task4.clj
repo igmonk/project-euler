45a92af686770850007867084f94ea63ec2366b1
Igor <Igor@Igors-MacBook-Pro.local>
2015-11-09 00:22:29 +0300

    Task 4 optimization.

--- src/project_euler/task4.clj
+++ src/project_euler/task4.clj
@@ -1,16 +1,42 @@
 (ns project-euler.task4)
 
-(defn palindrome?
+(defn- reverse-number
+  "Returns reversed number."
+  ([source]
+    (reverse-number source 0))
+  ([source reversed]
+    (if (= source 0)
+      reversed
+      (reverse-number (quot source 10)
+                      (+ (* reversed 10) (rem source 10))))))
+
+(defn- palindrome?
   "Determines whether a number is a palindrome."
   [x]
-  (let [s (str x)]
-    (= s (clojure.string/reverse s))))
+  (= x (reverse-number x)))
+
+(defn- numbers
+  "Returns lazy sequence of 3-digit numbers with descending order."
+  ([] (numbers 100))
+  ([to] (take-while (partial <= to) (iterate dec 999))))
+
+(defn- numbers-optimized
+  "Returns lazy sequence of 3-digit numbers with descending order.
+  All members will have a factor of 11.
+  p = 100000x + 10000y + 1000z + 100z + 10y + x = 11(9091x + 910y + 100z)"
+  [to]
+  (take-while (partial <= to) (iterate (fn [x] (- x 11)) 990)))
 
 (defn largest-palindrome
   "Returns the largest palindrome made from the product of two 3-digit numbers."
   []
-  (reduce max
-          (filter palindrome?
-                  (for [x (rseq (vec (range 100 1000)))
-                        y (rseq (vec (range 100 1000)))]
-                    (* x y)))))
+  (reduce (fn [palindrome-max a]
+            (reduce (fn [p-max b]
+                      (let [ab (* a b)]
+                        (if (palindrome? ab) ab p-max)))
+                    palindrome-max
+                    (take-while (partial < (/ palindrome-max a)) (if (= (mod a 11) 0)
+                                                                   (numbers a)
+                                                                   (numbers-optimized a)))))
+          0
+          (numbers)))
