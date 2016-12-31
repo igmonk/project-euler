20a7407b16ff2c70963dc5b7bdfc746a4c07ed4f
Igor <imoiseyenko93@gmail.com>
2016-12-31 07:53:57 +0300

    Task 6: Sum square difference.

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -3,12 +3,11 @@
   (:require [project-euler.task1 :as task1])
   (:require [project-euler.task2 :as task2])
   (:require [project-euler.task3 :as task3])
-  (:require [project-euler.task4 :as task4]))
+  (:require [project-euler.task4 :as task4])
+  (:require [project-euler.task6 :as task6]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  "(prn (task1/get-sum))"
-  "(prn (task2/even-fibs))"
-  (prn (task4/largest-palindrome)))
+  (prn (task6/execute)))
