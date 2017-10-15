03e9efc738e0ab479b02f07c671c9f50199af305
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-10-16 02:49:09 +0300

    Removing of unnecessary code.

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -1,13 +1,7 @@
 (ns project-euler.core
-  (:gen-class)
-  (:require [project-euler.task1 :as task1])
-  (:require [project-euler.task2 :as task2])
-  (:require [project-euler.task3 :as task3])
-  (:require [project-euler.task4 :as task4])
-  (:require [project-euler.task6 :as task6]))
+  (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
-  (prn (task6/execute)))
+  (println "Hello, World!"))
